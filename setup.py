9e898dc60e5c696fa542da542f92e0aa517b6f60
Maarten Broekman <maarten@broekman.us>
2022-06-25 15:30:18 -0400

    mjbroekman - new package and setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-# setup.py file for pymek package
+"""
+setup.py file for pysweeper package
+"""
 import setuptools
+
+dependencies = [ 'emoji', 'colorama' ]
 
 setuptools.setup(
     name="pysweeper",
-    version="1.0.0",
+    version="1.0.1",
+    author="Maarten J Broekman",
+    url="https://github.com/mjbroekman/python-minesweeper",
     description="CLI MineSweeper game",
     license="GPL-3.0 - https://opensource.org/licenses/GPL-3.0",
-    packages=setuptools.find_packages('src'),
-    package_dir={'': 'src'})
+    package_dir={'':'src/pysweeper'}
+)
